d9ec4c4673f41de71c228a253127647ec748bc2d
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-10-20 17:19:14 +0000

    etw

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
+'use strict'
+
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
     scope: "/ICS2O-PWA-Test/",
   })
 }
 
-;("use strict")
+function calculate () {
 
-function calculate() {
-  const length = parseInt(document.getElementById("pay").value)
-  const width = parseInt(document.getElementById("pay2").value)
+  const length = parseInt(document.getElementById('pay').value)
+  const width = parseInt(document.getElementById('pay2').value)
+  
 
-  const area = length * width * 0.18
-  const perimeter = length * width * (1.0 - 0.18)
+  const area = (length * width)* 0.18
+  const perimeter = (length * width)*(1.00 - 0.18)
   const TAX_RATE = 0.18
 
-  document.getElementById("area").innerHTML =
-    "The governemnt will take: " + area + "$"
-  document.getElementById("perimeter").innerHTML =
-    "your pay will be: " + perimeter + "$"
+  document.getElementById('area').innerHTML = 'The governemnt will take: ' + area + '$'
+  document.getElementById('perimeter').innerHTML = 'your pay will be: ' + perimeter + '$'
+  
 }
